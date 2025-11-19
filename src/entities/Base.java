6f1c6f2f9317755c7cf55fc1322fec4bdf044a0b
lucioperez01 <perezluciogabriel@gmail.com>
2025-11-19 11:25:24 -0300

    refactorizado de entidades con clase abstracta base

---
+++ src/entities/Base.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+/**
+ *
+ * @author lucioperez01
+ */
+public abstract class Base {
+    Long id;
+    boolean eliminado;
+    
+    public Base(){
+    };
+    
+    protected Base(Long id, boolean eliminado) {
+        this.id = id;
+        this.eliminado = eliminado;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public boolean isEliminado() {
+        return eliminado;
+    }
+    
+    public void setEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+    }
+}
--- src/entities/DispositivoIoT.java
+++ src/entities/DispositivoIoT.java
@@ -1,89 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package entities;
 
-/**
- *
- * @author Usuario
- */
-public class DispositivoIoT {
-    private Long id;
-    private boolean eliminado;
-    private String serial, modelo, ubicacion, firmwareVersion;
-    private ConfiguracionRed configuracionRed; // 1→1 unidireccional
-    
-    public DispositivoIoT() {}
-    
-    public DispositivoIoT(Long id, boolean eliminado, String serial, String modelo,
-        String ubicacion, String firmwareVersion, ConfiguracionRed conf) {
-        this.id = id;
-        this.eliminado = eliminado;
+public class DispositivoIoT extends Base {
+    private String serial;
+    private String modelo;
+    private String ubicacion;
+    private String firmwareVersion;
+
+    // Relación 1→1 unidireccional
+    private ConfiguracionRed configuracionRed;
+
+    // Constructor vacío (requerido)
+    public DispositivoIoT() { 
+    }
+
+    // Constructor completo
+    public DispositivoIoT(long id, boolean eliminado, String serial, String modelo,
+                          String ubicacion, String firmwareVersion,
+                          ConfiguracionRed configuracionRed) {
+
+        super(id, eliminado);
         this.serial = serial;
         this.modelo = modelo;
         this.ubicacion = ubicacion;
         this.firmwareVersion = firmwareVersion;
-        this.configuracionRed = conf;
+        this.configuracionRed = configuracionRed;
     }
+    
+    // Constructor simple
     
     public DispositivoIoT(String serial, String modelo, String ubicacion, String firmwareVersion) {
     this.serial = serial;
     this.modelo = modelo;
     this.ubicacion = ubicacion;
     this.firmwareVersion = firmwareVersion;
-    this.eliminado = false;
+    this.eliminado = false;   // por defecto
 }
-    
-    //-------------------------------
-    // setters/getters
 
-    public void setId(Long id) {
-        if(id !=null) {
-            this.id = id;
-        }
-    }
-
-    public void setEliminado(boolean eliminado) {
-        this.eliminado = eliminado;
-    }
+    // ---------------------------
+    // Getters y Setters
+    // ---------------------------
 
     public void setSerial(String serial) {
-        if(serial != null){
-            this.serial = serial;
-        }
+        this.serial = serial;
     }
 
     public void setModelo(String modelo) {
-        if(modelo != null) {
-            this.modelo = modelo;
-        }
+        this.modelo = modelo;
     }
 
     public void setUbicacion(String ubicacion) {
-        if(ubicacion != null) {
-            this.ubicacion = ubicacion;    
-        }
+        this.ubicacion = ubicacion;
     }
 
     public void setFirmwareVersion(String firmwareVersion) {
-        if(firmwareVersion != null) {
-            this.firmwareVersion = firmwareVersion;
-        }
+        this.firmwareVersion = firmwareVersion;
     }
 
     public void setConfiguracionRed(ConfiguracionRed configuracionRed) {
-        if(configuracionRed != null) {
-            this.configuracionRed = configuracionRed;
-        }
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public boolean isEliminado() {
-        return eliminado;
+        this.configuracionRed = configuracionRed;
     }
 
     public String getSerial() {
@@ -105,15 +79,17 @@ public class DispositivoIoT {
     public ConfiguracionRed getConfiguracionRed() {
         return configuracionRed;
     }
-    
-    
-    //toString
 
     @Override
     public String toString() {
-        return "DispositivoIoT{" + "id=" + id + ", eliminado=" + eliminado + ", serial=" + serial + ", modelo=" + modelo + ", ubicacion=" + ubicacion + ", firmwareVersion=" + firmwareVersion + ", configuracionRed=" + configuracionRed + '}';
+        return "DispositivoIoT { " +
+                "id=" + id +
+                ", eliminado=" + eliminado +
+                ", serial='" + serial + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", ubicacion='" + ubicacion + '\'' +
+                ", firmwareVersion='" + firmwareVersion + '\'' +
+                ", configuracionRed=" + configuracionRed +
+                " }";
     }
-    
-    
-    
 }
